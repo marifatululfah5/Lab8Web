e6ec11218bbd00f88bd46a13b6bc31f72a1a3fa3
marifatul ulfah <marifatululfah99@gmail.com>
2022-05-31 20:54:49 +0700

    add index2.php

---
+++ index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet"
+media="screen" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi praktikum 8</h1>
+        </header>
+    <nav>
+        <a href="koneksi.php">Koneksi</a>
+        <a href="tambah.php">Tambah</a>
+        <a href="ubah.php">Ubah</a>
+        <a href="hapus.php">hapus</a>
+        
+</nav>
